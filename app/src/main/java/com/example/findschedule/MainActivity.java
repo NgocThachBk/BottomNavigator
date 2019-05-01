48a46cc1df4e44647f00474439cc3c85de3e96a1
thachpn03tcdb@gmail.com <Ik29250708>
2019-05-01 15:12:55 +0700

    ver1

---
+++ app/src/main/java/com/example/findschedule/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.findschedule;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.FrameStats;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //toolbar.setTitle("Test");
+
+
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected( MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_search:
+                    //toolbar.setTitle("Search");
+                    return true;
+                case R.id.navigation_add:
+                    fragment = new Note();
+                    loadFragment(fragment);
+                    //toolbar.setTitle("Add");
+                    return true;
+                case R.id.navigation_note:
+                    //toolbar.setTitle("Note");
+                    return true;
+                case R.id.navigation_user:
+                    //toolbar.setTitle("User");
+                    return true;
+            }
+
+            return false;
+        }
+    };
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
